aa95876872d45b15dc4426e75df17d07c749b005
Gustavo Machado <gustavomachado.521@gmail.com>
2026-02-07 16:50:28 -0300

    Project Structure

--- script/search.js
+++
@@ -1,8 +0,0 @@
-const mes = document.getElementsByClassName("items")
-const jan = document.getElementById("Fevereiro")
-
-mes[0].addEventListener('click', () =>{
-    if(jan.id == "Fevereiro"){
-        jan.style.display = 'none'
-    }
-})
